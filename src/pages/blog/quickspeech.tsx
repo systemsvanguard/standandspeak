51231e20b3df15571125a3ba7994102db7b6995d
Ryan Hunter <36390187+systemsvanguard@users.noreply.github.com>
2025-05-14 07:55:04 -0400

    Blog Article Upload
    Blog Article Upload

--- src/pages/blog/quickspeech.tsx
+++ src/pages/blog/quickspeech.tsx
@@ -10,8 +10,33 @@ const QuickSpeech = () => {
         className="img-fluid mb-4 rounded"
       />
       <h1>How to Write a Speech Quickly?</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat, urna nec pretium sagittis...</p>
-      <p>Curabitur blandit tempus porttitor. Etiam porta sem malesuada magna mollis euismod...</p>
+      <p>Here are a couple techniques that have helped me (1) think of speech topics quickly, and (2) write speech topics really quickly ... like within hours!  YMWV ~ Your Mileage Will Vary ... BUT I am certain there are some helpful tips here.  Enjoy!</p>
+      <p>The first technique I learned from my Toastmaster mentor and friend Leslie B.  When trying to think of a topic to make a speech on ... use "The Happy Intersection" of these 3 Circles, much like a Venn Diagram.  </p>
+      <img src={`/images/venn_diagram.webp`} alt="Venn Diagram" className="img-fluid mb-3 rounded" style={{ maxWidth: '271px' }} /><br />
+      <ol>
+        <li>In the first circle, "A", think of topics that you love and are extremely passionate about.  At a cocktail party, or when a strager asks you "what you do for fun", these are the things you talk about the first and the longest.  For most people its a hobby, or something they get fun and enjoyment from.  <strong>Because you love these, it will be easy for you to speak on these topics.</strong>.  If the topics don't come to mind readily, try taking a long walk, or doing something to relax yourself.  </li>
+        <li>In the second circle, "B", are <strong>things you know very well</strong>. For many peple, this will be something related to their work/ profession, or something they have a lot of experience with.  Thus, it will be relatively easy for your to research on this topic as needed for the speech.</li>
+        <li>In the third circle, "C", add in <strong>things that your audience cares deeply about</strong> ... things that are of interest to your audience.  If you are speaking to a Toastmasters group/ club, the probability is high that they will care about (1) making speeches confidently, (2) improving their skills, (3) learning new things, and learning organizational, leadership, "soft skills", and/ or communication techniques.  Spent sometime thinking deeply about this.  I find that spending time researching the target audience, tends to pay huge dividends.</li>
+      </ol>
+      <p>Now comes the tricky part.  You want to find a topic that neatly fits into all three circles.  A topic that you love, you can easily research, AND your audience eagerly wants to hear about it.  This may take a few tries, BUT gets exponentially better with practice. </p>
+
+      <img src={`/images/confident_lady_01.webp`} alt="Confident Speakers" className="img-fluid mb-3 rounded" style={{ maxWidth: '650px', height: '364px' }} /><br />
+      
+
+      <p className="text-primary">Here's another technique that has helped me well. </p>
+      <ol>
+        <li>Start first with the WHAT ... a quick definition 7 layman's explanation of the topic being discussed.</li>
+        <li>Next, move on to the WHY and/ or WiiFM ... "What's in it For Me" for your audience.  WHY is it important to know this topic?  How will it help them?  Discuss that <strong>briefly!</strong></li>
+        <li>Practical Tips/ "How To" / Techniques, and "Takeaways".  List out a few important tips about the topic, that would greatly assist the listener, or increase ther chances of success with the task at hand.  Try to stick to a max of 3-5 tips; most people tend to remember only the most relevant or relatable 3 tips.  </li>
+        <li>Add a brief personal story or relatable experience.  Humans are hardwired to enjoy hearing "stories", and this is your chance to make the story truly your own, by relating it back to a personal experience you had, that you can share in a quick story. </li>
+        <li>Summarize, and state your <strong>"Call To Action"</strong> (CTA) ... the thing that you want your audience to remember and act upon.  The CTA is normally heavily related to the core, underlying reason of why you chose this topic, and what you want to see accomplished by you giving the speech.</li>
+      </ol>
+
+      <p>I do hope these techniques help you, and you have as much fun using them, as I had sharing them with you.  Cheers! </p>
+      <p className="text-primary">Check back soon for more articles. </p>
+
+
+
 
       <br />
       <Link to="/" className="btn btn-outline-secondary mt-4">
